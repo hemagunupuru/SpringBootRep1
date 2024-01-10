577caa52ff4a2cc0a286166c0c5542522a5aae0b
hemagunupuru <113720209+hemagunupuru@users.noreply.github.com>
2024-01-10 16:25:19 +0530

    Update Controller1.java

--- src/main/java/com/example/demo/Controller1.java
+++ src/main/java/com/example/demo/Controller1.java
@@ -1,6 +1,31 @@
 package com.example.demo;
 
-public class Controller1
-{
-	
-}
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller1 {
+	@Autowired
+	Service1 es;
+	@PostMapping("/emp-ni")
+	public Map insertnData(@RequestBody Emploee_Pojo emp) {
+	Map <String,String> data =new HashMap<String,String>();
+	try {
+	int response=es.UpdateData(emp);
+
+	if(response>0)
+	data.put("1", "Data inserted");
+	else
+	data.put("2", "Data not inserted");
+	}
+	catch (Exception e) {
+	data.put("3", e.toString());
+	}
+	return data;
+
+	}
+	}
+
